2306a7a6a4436299189680cccc9949fdc0a7e945
Your Name <you@example.com>
2020-09-14 14:05:42 +0100

    Updates

---
+++ precache-manifest.ca9bf5a0e2b21d5d2d334be2c270e402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a83cf61f74fe33d76b1",
+    "url": "/reactapp/static/css/main.4abc18f3.chunk.css"
+  },
+  {
+    "revision": "0a83cf61f74fe33d76b1",
+    "url": "/reactapp/static/js/main.076309bd.chunk.js"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "c98c98277484661a3f51",
+    "url": "/reactapp/static/js/2.d150306f.chunk.js"
+  },
+  {
+    "revision": "ad360679ac14e9074a5d0507e598bcdb",
+    "url": "/reactapp/index.html"
+  }
+];
